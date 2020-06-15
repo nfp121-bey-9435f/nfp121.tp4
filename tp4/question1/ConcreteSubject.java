609e1e9cb6ab0a1999f2e606e4d02f298de6ff3d
Abou Karam Sami <sami.aboukaram.1@isae.edu.lb>
2020-06-15 21:40:30 +0300

    Premiere Modification

--- tp4/question1/ConcreteSubject.java
+++ tp4/question1/ConcreteSubject.java
@@ -1,31 +1,31 @@
-package question1;
-
-import java.util.Observable;
-import java.util.ArrayList;
-
-/**
- * Décrivez votre classe ConcreteSubject ici.
- * 
- * @author (votre nom)
- * @version (un numéro de version ou une date)
- */
-public class ConcreteSubject extends Observable {
-
-	/** ConcreteSubject est composé d'une liste list */
-	private ArrayList<String> list;
-
-	public ConcreteSubject() {
-		list = new ArrayList<String>();
-	}
-
-	public void insert(String name) {
-		list.add(name);
-		setChanged();
-		notifyObservers(name);
-	}
-
-	public String toString() {
-		return list.toString();
-	}
-
-}
+    package question1;
+    
+    import java.util.Observable;
+    import java.util.ArrayList;
+    
+    /**
+     * Décrivez votre classe ConcreteSubject ici.
+     * 
+     * @author Sami Abou Karam
+     * @version June 2020
+     */
+    public class ConcreteSubject extends Observable {
+    
+    	/** ConcreteSubject est composé d'une liste list */
+    	private ArrayList<String> list;
+    
+    	public ConcreteSubject() {
+    		list = new ArrayList<String>();
+    	}
+    
+    	public void insert(String name) {
+    		list.add(name);
+    		setChanged();
+    		notifyObservers(name);
+    	}
+    
+    	public String toString() {
+    		return list.toString();
+    	}
+    
+    }
